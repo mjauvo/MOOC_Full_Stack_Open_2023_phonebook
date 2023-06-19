70683680f20fc2578af4fa23bd6768df2a7090e6
Markus Auvo <markus.auvo@helsinki.fi>
2023-06-20 01:13:08 +0300

    This commit comprises of exercise 3.10

--- client/src/components/FilterForm.js
+++
@@ -1,9 +0,0 @@
-const FilterForm = ({value, handleFilterChange}) => {
-    return (
-        <div>
-            Filter shown with<br/><input value = {value} onChange = {handleFilterChange}/>
-        </div>
-    )
-}
-
-export default FilterForm
--- client/src/components/Person.js
+++
@@ -1,9 +0,0 @@
-const Person = ({person, deletePerson}) => {
-    return (
-        <tr>
-            <td>{person.name}</td><td>({person.number})</td><td><button onClick={() => deletePerson(person.id)}>Delete</button></td>
-        </tr>
-    )
-}
-
-export default Person
