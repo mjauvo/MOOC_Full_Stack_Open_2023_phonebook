3b1845d0a516d6e2b5680768cc259b1c10eac920
Markus Auvo <markus.auvo@helsinki.fi>
2023-11-18 13:47:48 +0200

    This commit comprises of exercise 3.13

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(morgan(':method :url :status  :res[content-length] - :response-time ms :
 app.use(cors())
 app.use(express.static('build'))
 
-const home = `<h1>Hello World!</h1><h3>This is Phonebook App (exercise 3.12)</h3>`
+const home = `<h1>Hello World!</h1><h3>This is Phonebook App (exercise 3.14)</h3>`
 
 // Show homepage
 app.get('/', (request, response) => {
@@ -41,95 +41,6 @@ app.get('/api/persons', (request, response) => {
     Person.find({}).then(persons => {
         response.json(persons)
     })
-})
-
-//Fetch and show a person
-app.get('/api/persons/:id', (request, response) => {
-    Person.findById(request.params.id).then(person => {
-        response.json(person)
-    })
-
-    console.log('GET request: /api/persons/', id)
-
-    if (person) {
-        console.log('200 SUCCESS')
-        const person_info = `<p><strong>${person.name}</strong><br/>
-        ${person.number}</p>`
-        response.send(person_info)
-    }
-    else {
-        console.log('400 NOT FOUND')
-        const error_404 = `<p><strong>404</strong><br>"This is not the person you are looking for ..."</p>`
-        response.status(404)
-        response.send(error_404).end()
-    }
-})
-
-// Delete a person
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-    //console.log('DELETE request: /api/persons/', id)
-    response.status(204).end()
-})
-
-const generateRandomId = () => {
-    const max = 10000
-    return Math.floor(Math.random() * max)
-}
-
-// Add a person
-app.post('/api/persons', (request, response) => {
-    const body = request.body
-    //console.log('POST request')
-
-    // name is missing -- 400 Bad Request
-    if (body.name == undefined) {
-        //console.log("404 Bad Request")
-        return response.status(400).json({
-            error: 'name missing'
-        })
-    }
-    // number is missing -- 400 Bad Request
-    if (body.name == undefined) {
-        //console.log("404 Bad Request")
-        return response.status(400).json({
-            error: 'number missing'
-        })
-    }
-    // name already exists -- 409 Conflict
-    const foundPerson = persons.find((person) => person.name === body.name)
-
-    if (foundPerson) {
-        //console.log("409 Conflict")
-        return response.status(409).json({
-            error: 'name already exists'
-        })
-    }
-
-    else {
-        const person = {
-            name: body.name,
-            number: body.number,
-            id: generateRandomId()
-        }
-
-        person
-        .save()
-        .then(savedPerson => {
-            response.json(savedPerson)
-        })
-    }
-/*
-    const person = {
-        name: body.name,
-        number: body.number,
-        id: generateRandomId()
-    }
-*/
-    //console.log('POST request: /api/persons/', person.id)
-    persons = persons.concat(person)
-    response.json(person)
 })
 
 // ----------------------------------------
